a6dbb6ce65a1395923a7ffa1e2baf9a725347e42
Dilia <andhalaya@hotmail.com>
2024-05-21 23:44:15 +0200

    added button

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,35 +1,39 @@
 import React from 'react';
 
-export default function SideBar({ filters, onFilterChange, onRandomize }) {
-  return (
-    <div className='sideBar'>
-        <h3>Select a group</h3>
-      <label>
-        <input
-          type='checkbox'
-          checked={filters.hiragana}
-          onChange={() => onFilterChange('hiragana')}
-        />
-        Hiragana
-      </label>
-      <label>
-        <input
-          type='checkbox'
-          checked={filters.katakana}
-          onChange={() => onFilterChange('katakana')}
-        />
-        Katakana
-      </label>
-      <label>
-        <input
-          type='checkbox'
-          checked={filters.kanji}
-          onChange={() => onFilterChange('kanji')}
-        />
-        Kanji
-      </label>
-      <button onClick={onRandomize}>Randomize</button>
-    </div>
-  );
+export default function SideBar({ filters, showAll, onFilterChange, onRandomize }) {
+    return (
+        <div className='sideBar'>
+            <h3>Select a group</h3>
+            <label>
+                <input
+                    type='checkbox'
+                    checked={filters.hiragana}
+                    onChange={() => onFilterChange('hiragana')}
+                />
+                Hiragana
+            </label>
+            <label>
+                <input
+                    type='checkbox'
+                    checked={filters.katakana}
+                    onChange={() => onFilterChange('katakana')}
+                />
+                Katakana
+            </label>
+            <label>
+                <input
+                    type='checkbox'
+                    checked={filters.kanji}
+                    onChange={() => onFilterChange('kanji')}
+                />
+                Kanji
+            </label>
+            <div style={{ display: 'flex', gap: '20px' }}>
+                <button onClick={showAll}>Show all</button>
+                <button onClick={onRandomize}>Randomize</button>
+            </div>
+
+        </div>
+    );
 }
 
